2c4837e0c59bfdb6d08c5a2c57b53217423ce6db
Yunho Cho <yhjc2692@gmail.com>
2025-07-17 23:45:57 -0400

    Added data download script

---
+++ downloads/download.sh
@@ -0,0 +1,6 @@
+gdown --folder https://drive.google.com/drive/folders/1EhvyNCAWWto6vMt0vXWMKBoSdYR_9pC2?usp=sharing
+unzip downloads/3d_front_processed.zip
+unzip downloads/3D-FUTURE-model-processed.zip
+unzip downloads/objautoencoder_pretrained.zip
+unzip downloads/pretrained_diffusion.zip
+unzip downloads/UncondGen.zip
